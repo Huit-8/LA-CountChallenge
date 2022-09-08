9f34a022a063eba19d53383823e8aa6fa804cd2e
Kaito Dogi <kendobu0405@gmail.com>
2022-09-08 22:24:10 +0900

    fix: newInstance の使用例を追記

--- app/src/main/java/app/aoyama/huit/countchallenge/ViewPagerAdapter.kt
+++ app/src/main/java/app/aoyama/huit/countchallenge/ViewPagerAdapter.kt
@@ -15,7 +15,9 @@ class ViewPagerAdapter (fragmentActivity: FragmentActivity):
     override fun createFragment(position: Int): Fragment =
         when (position) {
             0 -> Fragment1()
-            1 -> Fragment2()
+            // Doggy: Fragment2 で定義された newInstance を使用して Fragment2 を生成すると以下のようになる！
+            1 -> Fragment2.newInstance(param1 = "引数1つ目", param2 = "引数2つ目")
+            // 1 -> Fragment2()
             2 -> Fragment3()
             else -> throw IllegalArgumentException()
         }
