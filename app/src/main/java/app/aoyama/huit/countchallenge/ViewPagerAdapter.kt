225d665c2d22818b9cac6b058be43c0085f094e2
Kaito Dogi <kendobu0405@gmail.com>
2022-09-08 22:05:11 +0900

    review: ViewPagerAdapter

--- app/src/main/java/app/aoyama/huit/countchallenge/ViewPagerAdapter.kt
+++ app/src/main/java/app/aoyama/huit/countchallenge/ViewPagerAdapter.kt
@@ -4,16 +4,19 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
+// Doggy: ViewPagerAdapter だと、どの ViewPager の adapter か分からないので、
+//  TitleViewPagerAdapter や TutorialViewPagerAdapter のような命名がより良さそう！
 class ViewPagerAdapter (fragmentActivity: FragmentActivity):
     FragmentStateAdapter(fragmentActivity){
 
     override fun getItemCount(): Int = 3
 
+    // Doggy: このあたりも良い感じにスペースを入れられると読みやすさ（可読性）が上がりそう！
     override fun createFragment(position: Int): Fragment =
-        when(position){
-            0->Fragment1()
-            1->Fragment2()
-            2->Fragment3()
+        when (position) {
+            0 -> Fragment1()
+            1 -> Fragment2()
+            2 -> Fragment3()
             else -> throw IllegalArgumentException()
         }
 }
