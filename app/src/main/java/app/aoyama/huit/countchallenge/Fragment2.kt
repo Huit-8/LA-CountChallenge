6db42541d87adb7aa01eae1119d4a245a082a498
Kaito Dogi <kendobu0405@gmail.com>
2022-09-08 22:22:15 +0900

    comment: Fragment2

--- app/src/main/java/app/aoyama/huit/countchallenge/Fragment2.kt
+++ app/src/main/java/app/aoyama/huit/countchallenge/Fragment2.kt
@@ -6,23 +6,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+// Doggy: せっかくなので、 Fragment で自動生成されるコードに、
+//  補足でコメントを書いてみたので、ぜひ読んでみよう！
+
+// Doggy: Fragment2 に渡す値のキー（SharedPreferences や Intent の putExtra/getXXExtra と同様）
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [Fragment2.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Fragment2 : Fragment() {
-    // TODO: Rename and change types of parameters
+    // Doggy: Fragment2 に、 param1, param2 という String 型の変数を用意する。
+    //  Kotlin は null を基本的に使わない言語なので、
+    //  デフォルトで生成されるコードとは言え、この変数の定義はあまり真似したくない！
     private var param1: String? = null
     private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Doggy: param1, param2 を、 Fragment2 を生成した時に渡される値で初期化する。
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
@@ -33,23 +33,19 @@ class Fragment2 : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+        // Doggy: layout ファイル（R.layout.fragment_2） を Fragment2 で使用できるようにする。
         return inflater.inflate(R.layout.fragment_2, container, false)
     }
 
+    // Doggy: companion object について調べてみよう！（応用）
+    //  よく分からなかったら、オブジェクトやクラス、インスタンスについて調べてみよう！
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Fragment2.
-         */
-        // TODO: Rename and change types and number of parameters
+        // Doggy: @JvmStatic について調べてみよう！（応用）
+        //  よく分からなかったら、アノテーションについて調べてみよう！
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             Fragment2().apply {
+                // Doggy: Fragment を生成するときに ARG_PARAM1, ARG_PARAM2 というキーで値を渡す。
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
